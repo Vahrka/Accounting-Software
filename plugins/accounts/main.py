3af9d11bc57191ce92e81a32d4a9a7b2aab7fe39
Bambier <nimpel2@proton.me>
2025-03-21 10:11:55 +0330

    Make Test plugin

---
+++ plugins/accounts/main.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QPushButton
+
+from src.core.plugins.plugin_base import PluginBase
+
+
+class Account(PluginBase):
+
+    def register(self):
+        self.button = QPushButton(self.tr("Push me"))
+        self.main_window.add_core_navs(self.button)
