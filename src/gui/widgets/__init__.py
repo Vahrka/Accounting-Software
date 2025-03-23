1af10c484cf0e48c92709d230ae531bddb53eb20
Bambier <nimpel2@proton.me>
2025-03-23 22:46:29 +0330

    Remove unused files

--- src/gui/widgets/__init__.py
+++ src/gui/widgets/__init__.py
@@ -1,13 +1,7 @@
-from src.gui.widgets.accounts import AccountsWidget
 from src.gui.widgets.base_screen import BaseScreen
-from src.gui.widgets.dashboard import DashboardWidget
-from src.gui.widgets.ledger import LedgerWidget
 from src.gui.widgets.menubar import Menubar
 
 __all__ = [
-    'Menubar',
     'BaseScreen',
-    'DashboardWidget',
-    'AccountsWidget',
-    'LedgerWidget',
+    'Menubar',
 ]
