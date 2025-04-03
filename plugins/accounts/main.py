103350bd1e93e2e4d6ec8b41facd8bb13cdd910c
Bambier <nimpel2@proton.me>
2025-04-04 00:52:59 +0330

    Fix PluginBase class from plugins.

--- plugins/accounts/main.py
+++ plugins/accounts/main.py
@@ -1,9 +1,13 @@
-from PySide6.QtWidgets import QPushButton
+from PySide6.QtWidgets import QPushButton, QWidget
 
 from plugins.accounts.account_screen import AccountScreen
 
 
-class Account:
+class Account(QWidget):
+    def __init__(self, add_func: callable = None, remove_func: callable = None):
+        super().__init__()
+        self.__add_to_screen = add_func
+        self.__remove_plugin_btn_screen = remove_func
 
     def register(self):
         button = QPushButton(self.tr("Account"))
@@ -15,3 +19,15 @@ class Account:
 
     def unregister(self):
         self.remove("Account")
+
+    def add(self, btn: QPushButton, screen: QWidget):
+        self.__add_to_screen(btn, screen)
+
+    def remove(self, name: str):
+        self.__remove_plugin_btn_screen(name)
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}Plugin >"
+
+    def __repr__(self):
+        return self.__str__()
--- plugins/sales/main.py
+++ plugins/sales/main.py
@@ -1,9 +1,13 @@
-from PySide6.QtWidgets import QPushButton
+from PySide6.QtWidgets import QPushButton, QWidget
 
 from .sales_screen import SalesScreen
 
 
-class Sales:
+class Sales(QWidget):
+    def __init__(self, add_func: callable = None, remove_func: callable = None):
+        super().__init__()
+        self.__add_to_screen = add_func
+        self.__remove_plugin_btn_screen = remove_func
 
     def register(self):
         sales_screen = SalesScreen()
@@ -15,3 +19,15 @@ class Sales:
 
     def unregister(self):
         self.remove("Sales")
+
+    def add(self, btn: QPushButton, screen: QWidget):
+        self.__add_to_screen(btn, screen)
+
+    def remove(self, name: str):
+        self.__remove_plugin_btn_screen(name)
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}Plugin >"
+
+    def __repr__(self):
+        return self.__str__()
